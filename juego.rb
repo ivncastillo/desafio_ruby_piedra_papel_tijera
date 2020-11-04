a1d77a6d1104907d000d75c154d2bb3879083247
ivncastillo <ivncastillo@gmail.com>
2020-11-04 20:56:59 -0300

    final

--- juego.rb
+++ juego.rb
@@ -1,49 +1,86 @@
-humano = ARGV[0].chomp
-if humano != 'piedra' && humano != 'papel' && humano != 'tijera'
-    print 'Argumento inválido: Debe ser piedra, papel o tijera.'
-else
-    aleatorio=rand(3)
 
-    if aleatorio==0
-        computador='piedra'
-    end
-    if aleatorio==1
-        computador='papel'
-    end
-    if aleatorio==2    
-        computador='tijera'
-    end
 
-    puts "Computador juega #{computador}" 
+puts 'Turno jugador uno'
+puts '1. Piedra'
+puts '2. Papel'
+puts '3. Tijeras'
+puts '4. Salir'
+puts 'Jugador 1 elija una opción 1,2,3 o 4'
 
-    if humano=='piedra' && computador=='piedra'
-        print "Empataste"
-    end
-    if humano=='piedra' && computador=='papel'
-        print "Perdiste"
-    end
-    if humano=='piedra' && computador=='tijera'
-        print "Ganaste"
-    end
+jugador1 = gets.chomp.to_i
 
-    if humano=='papel' && computador=='piedra'
-        print "Ganaste"
-    end
-    if humano=='papel' && computador=='papel'
-        print "Empataste"
-    end
-    if humano=='papel' && computador=='tijera'
-        print "Perdiste"
-    end
 
-    if humano=='tijera' && computador=='piedra'
-        print "Perdiste"
-    end
-    if humano=='tijera' && computador=='papel'
-        print "Ganaste"
-    end
-    if humano=='tijera' && computador=='tijera'
-        print "Empataste"
-    end
+while jugador1!=1 && jugador1!=2 && jugador1!=3 && jugador1!=4
+    puts 'Argumento inválido: Debe ser una opción 1, 2 ,3 o 4'
+    puts 'Turno jugador uno'
+    puts '1. Piedra'
+    puts '2. Papel'
+    puts '3. Tijeras'
+    puts '4. Salir'
+    puts 'Elija una opción 1,2,3 o 4'
 
+    jugador1 = gets.chomp.to_i
 end
+
+if jugador1==4
+    puts 'el jugador 1 se va, el juego termina'
+
+elsif jugador1==1 || jugador1==2 || jugador1==3
+    puts 'Turno jugador dos'
+    puts '1. Piedra'
+    puts '2. Papel'
+    puts '3. Tijeras'
+    puts '4. Salir'
+
+    puts 'Jugador 2 elija una opción 1,2,3 o 4'
+
+    jugador2 = gets.chomp.to_i
+
+    while jugador2!=1 && jugador2!=2 && jugador2!=3 && jugador2!=4
+        puts 'Argumento inválido: Debe ser una opción 1, 2 ,3 o 4'
+        puts 'Turno jugador dos'
+        puts '1. Piedra'
+        puts '2. Papel'
+        puts '3. Tijeras'
+        puts '4. Salir'
+        puts 'Elija una opción 1,2,3 o 4'
+    
+        jugador2 = gets.chomp.to_i
+    end
+
+    if jugador2==4
+    puts 'el jugador 2 se va, el juego termina'
+    elsif jugador2==1 || jugador2==2 || jugador2==3
+
+        if jugador1==1 && jugador2==1
+            print "Empate"
+        end
+        if jugador1==1 && jugador2==2
+            print "Gana Jugador 2"
+        end
+        if jugador1==1 && jugador2==3
+            print "Gana Jugador 1"
+        end
+
+        if jugador1==2 && jugador2==1
+            print "Gana Jugador 1"
+        end
+        if jugador1==2 && jugador2==2
+            print "Empate"
+        end
+        if jugador1==2 && jugador2==3
+            print "Gana Jugador 2"
+        end
+
+        if jugador1==3 && jugador2==1
+            print "Gana Jugador 2"
+        end
+        if jugador1==3 && jugador2==2
+            print "Gana Jugador 1"
+        end
+        if jugador1==3 && jugador2==3
+            print "Empate"
+        end
+    end
+end
+
